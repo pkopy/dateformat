407a1a7763b278f707e862e7beda0dc1b931d123
pkopy <pawel.kopycki@gmail.com>
2020-04-14 15:53:36 +0200

    added test

--- dateFormat.js
+++ dateFormat.js
@@ -1,76 +1,78 @@
-
 // "YYYY/MM/DD"
 //
-module.exports  =
-class  MyDate{
-    constructor(date){
-        this.date = date ? date: new Date(Date.now());
-        console.log('init date: ', this.date)
-    }
-
-    addZero (number) {
-        return number < 10 ? '0'+number.toString() : number.toString();
-    }
-
-    dateFormat(date, format, separator) {
-        if (!date) date = this.date;
-        let dateNumber = 0;
-        let day = 0;
-        let month = 0;
-        let year = 0;
-        let result = {
-            M: this.addZero(month),
-            D: this.addZero(dateNumber),
-            Y: this.addZero(year)
-        };
-        let rr = ''
+module.exports = {
 
 
-        if (!separator) separator = '/'
-
-        if (date instanceof Date) {
-            // for (let i of Object.keys(date)) {
-            //     console.log(i)
-            // }
-            // console.log(date.constructor)
-            result.D = this.addZero(date.getDate());
-            day = date.getDay();
-            result.M = this.addZero(date.getMonth() + 1);
-            result.Y = this.addZero(date.getFullYear());
-
-            console.log(date)
-        } else {
-            console.log(typeof date)
+    MyDate: class MyDate {
+        constructor(date) {
+            this.date = date ? date : new Date(Date.now());
+            console.log('init date: ', this.date)
         }
 
-        if (format && typeof format === 'string' && format.length === 3) {
+        addZero(number) {
+            return number < 10 ? '0' + number.toString() : number.toString();
+        }
+
+        dateFormat(date, format, separator) {
+            if (!date) date = this.date;
+            let dateNumber = 0;
+            let day = 0;
+            let month = 0;
+            let year = 0;
+            let result = {
+                M: this.addZero(month),
+                D: this.addZero(dateNumber),
+                Y: this.addZero(year)
+            };
+            let rr = ''
 
 
-            const form = format.toUpperCase().split('');
-            console.log(form)
+            if (!separator) separator = '/'
 
-            for (let e of form) {
-                rr += result[e] + separator
+            if (date instanceof Date) {
+                // for (let i of Object.keys(date)) {
+                //     console.log(i)
+                // }
+                // console.log(date.constructor)
+                result.D = this.addZero(date.getDate());
+                day = date.getDay();
+                result.M = this.addZero(date.getMonth() + 1);
+                result.Y = this.addZero(date.getFullYear());
+
+                console.log(date)
+            } else {
+                console.log(typeof date)
             }
 
-            console.log("test:", rr.substring(0, rr.length - separator.length))
-        } else {
-            for (let e in result) {
-                rr += result[e] + separator
+            if (format && typeof format === 'string' && format.length === 3) {
+
+
+                const form = format.toUpperCase().split('');
+                console.log(form)
+
+                for (let e of form) {
+                    rr += result[e] + separator
+                }
+
+                console.log("test:", rr.substring(0, rr.length - separator.length))
+            } else {
+                for (let e in result) {
+                    rr += result[e] + separator
+                }
+                console.log(rr.substring(0, rr.length - separator.length))
             }
-            console.log(rr.substring(0, rr.length - separator.length))
+        }
+
+        addDay(num, separator) {
+            if (!separator) separator = '/'
+            let x = Date.parse(this.date) + (num * 24 * 60 * 60 * 1000)
+            this.date = new Date(x)
+            console.log()
+            this.dateFormat(new Date(x), 'ymd', separator)
         }
     }
 
-    addDay(num, separator)  {
-        if (!separator) separator = '/'
-        let x = Date.parse(this.date) + (num * 24 * 60 * 60 * 1000)
-        console.log()
-        this.dateFormat(new Date(x),'ymd', separator)
-    }
 }
-
-
 
 
 // const zz = new MyDate()
--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 const dateformater = require('./dateFormat')
 
-let y = new dateformater()
+let y = new dateformater.MyDate()
+y.addDay(-35, '--')
+y.dateFormat(null, 'dmy')
 console.log(y.addZero(5))
+console.log(y)
+
+console.log(new Date(Date.now()).getTimezoneOffset())
